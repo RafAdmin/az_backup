103d341d7e30e21a99086645418c29c1512a9c72
LevelUP Consultoria e Marketing <levelupconsultor@gmail.com>
2025-02-08 17:01:34 -0300

    backup

---
+++ frontend/build/precache-manifest.f7dd4104eb17fd378a5982face9b6ca4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb08e3ed047a2f4c9ac6de51799c2edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2b6561c2a3954c1234",
+    "url": "/static/css/2.406c6332.chunk.css"
+  },
+  {
+    "revision": "1f9b1b6364e41743b605",
+    "url": "/static/css/main.40de1c4b.chunk.css"
+  },
+  {
+    "revision": "1e2b6561c2a3954c1234",
+    "url": "/static/js/2.066099cc.chunk.js"
+  },
+  {
+    "revision": "dba69b44eaff91d1a3ef4fac665d9644",
+    "url": "/static/js/2.066099cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568c96fd7a722bb1518b",
+    "url": "/static/js/3.8d85018f.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.8d85018f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9b1b6364e41743b605",
+    "url": "/static/js/main.ee42edbf.chunk.js"
+  },
+  {
+    "revision": "841208533ad28ca33f28",
+    "url": "/static/js/runtime-main.7e79543c.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "d09b2574ef9c522322379d9321cbc4d1",
+    "url": "/static/media/logo.d09b2574.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
